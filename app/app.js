7377a916b7de56b268f3a8fe0d8dc8a2e2a525e7
Harry Nguyen <hpnguyen52@gmail.com>
2019-05-12 01:52:36 -0400

    added states

--- app/app.js
+++ app/app.js
@@ -1,11 +1,38 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
-let app = angular.module('hatomi',['ui.router',  'ngRoute', 'hatomi.view1', 'hatomi.view2', 'hatomi.version']);
+let app = angular.module('hatomi', ['ui.router', 'ngRoute', 'hatomi.view1', 'hatomi.view2', 'hatomi.version']);
 
-app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  var baseState = {
+    name: 'Base',
+    url: '',
+    component: 'base',
+    redirectTo: 'Base.Home'
+  }
+  var homeState = {
+    name: 'Base.Home',
+    url: '/Home',
+    component: 'home'
+  }
+  $stateProvider.state(baseState);
+  $stateProvider.state(homeState);
+  $urlRouterProvider.otherwise('/Home');
 }]);
+
+app.component("home",
+  {
+    templateUrl: '/states/home/home.template.html',
+    controller: homeController
+  });
+
+
+
+function homeController() {
+  var $ctrl = this;
+  $ctrl.person = { 'name': 'hatomi' };
+}
+
+app.controller('homeController', homeController);
+
 
---
+++ app/states/base/base.setup.js
@@ -0,0 +1,41 @@
+app.component("base",
+  {
+    templateUrl: '/states/base/base.template.html',
+    controller: baseController
+  });
+
+
+function baseController() {
+    var $ctrl = this;
+    $ctrl.person = { 'name': 'hatomi' };
+  
+    $ctrl.nav = [
+      {
+        title: 'Home',
+        uiSref: 'Base.Home',
+        hasSubnav: false
+      },
+      {
+        title: 'UX',
+        uiSref: 'Base.Works.UX',
+        hasSubnav: false,
+      },
+      {
+        title: 'Illustration',
+        uiSref: 'Base.Works.Illustration',
+        hasSubnav: false
+      },
+      {
+        title: 'Other',
+        uiSref: 'Base.Works.Other',
+        hasSubnav: false
+      },
+      {
+        title: 'Contact',
+        uiSref: 'Base.Contact',
+        hasSubnav: false
+      }
+    ];
+  }
+
+  app.controller('baseController', baseController);
